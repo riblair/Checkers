2932a6866c12546877d4366f032e08776e4282b4
PaulGodinez452 <89924917+PaulGodinez452@users.noreply.github.com>
2021-09-25 10:33:33 -0400

    Made King, Pawn and their tests

---
+++ src/King.cpp
@@ -0,0 +1,18 @@
+/*
+ * King.cpp
+ *
+ *  Created on: Sep 25, 2021
+ *      Author: paulg
+ */
+
+#include "King.h"
+
+King::King() {
+	// TODO Auto-generated constructor stub
+
+}
+
+King::~King() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Pawn.h
@@ -0,0 +1,31 @@
+/*
+ * Pawn.h
+ *
+ *  Created on: Sep 25, 2021
+ *      Author: paulg
+ */
+
+#ifndef SRC_PAWN_H_
+#define SRC_PAWN_H_
+#include <stdio.h>
+#include <stdbool.h>
+//reminder that (0,0) starts in the topleft
+typedef struct Position
+{
+	int row;
+	int col;
+}Position;
+
+
+class Pawn {
+public:
+	Pawn();
+	virtual ~Pawn();
+	void makeMove(Position* pos);
+	Position* returnPosition();
+	bool color; //true for black, false for red?
+private:
+	Position* pos;
+};
+
+#endif /* SRC_PAWN_H_ */
