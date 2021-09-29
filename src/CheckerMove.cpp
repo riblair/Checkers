bf5b2123346664618016071a54967b94567bde29
riblair <Ryandeathsquad@gmail.com>
2021-09-29 14:57:26 -0400

    takePawnAtPositon method written (untested), and isLegal methodology written

--- src/CheckerMove.cpp
+++ src/CheckerMove.cpp
@@ -16,7 +16,26 @@ CheckerMove::~CheckerMove() {
 	// TODO Auto-generated destructor stub
 }
 
-bool CheckerMove::isLegal() { //need to fix Stub once Board is updated
+bool CheckerMove::isLegal(Board* board, Position* pawn, Position* possibleMove) { //need to fix Stub once Board is updated
 	// TODO Make method after peices classes r created and Board is done
+
+	//Check if possibleMove pos is out of bounds,
+	//Check if a pawn is in possibleMove pos,
+		// if true, return false;
+	//Check if pawn is king
+		// if false
+			//if pawn is red && move is backwards
+				// return false
+			//if pawn is black && move is backwards
+				// return false
+
+	// if move is 1 space away, return true.
+	// else (if 2 move away)
+		// check if pawn exists in between pawn pos and possibleMove pos
+			// if a pawn exists && is of the opposite color,
+				// return true
+			// else return false
+
+
 	return false;
 }
--- src/CheckerMove.h
+++ src/CheckerMove.h
@@ -7,13 +7,17 @@
 
 #ifndef CHECKERMOVE_H_
 #define CHECKERMOVE_H_
+#include "Board.h"
+
 
 class CheckerMove {
 public:
 	CheckerMove();
 	virtual ~CheckerMove();
-	bool isLegal(); // board state, start pos (struct), end pos (struct),
+	bool isLegal(Board* board, Position* pawn, Position* possibleMove); // board state, start pos (struct), end pos (struct),
 	//mybe more office hours
+private:
+
 };
 
 
